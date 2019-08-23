b27d542ed16013a25b9f6aaf70234da228adc01a
Danillo Souza <danillosouza1704@gmail.com>
2019-08-23 10:36:27 -0300

    Idade exercise done

--- C/Lista1 ED1/encontro.c
+++ C/Lista1 ED1/encontro.c
@@ -41,20 +41,24 @@ int main(){
             saveVar[0] = paInitialX;
             saveVar[1] = paInitialY;
             saveVar[2] = i+1;
-            bool = 1;
+            bool+=1;
         }
-        else if (((paInitialX == pbInitialX) && (paInitialY == pbInitialY)) && bool == 0){
+        else if (((paInitialX < 1) || (paInitialY < 1)) && bool == 0)
+        {
             saveVar[0] = paInitialX;
             saveVar[1] = paInitialY;
-            saveVar[2] = i+1;
-            bool = 2;
+            saveVar[2] = i + 1;
+            bool+=3;
         }
     }
     if (bool == 0){
         printf("Nao se encontraram\n");
     }
-    else if(bool == 1){
+    else if(bool == 1 || bool == 4){
         printf("PA saiu na posicao (%d,%d) no passo %d\n", saveVar[0], saveVar[1], saveVar[2]);
+    }
+    else if (bool == 3) {
+        printf("PB saiu na posicao (%d,%d) no passo %d\n", saveVar[0], saveVar[1], saveVar[2]);
     }
     else{
         printf("Encontraram-se na posicao (%d,%d) no passo %d\n", saveVar[0], saveVar[1], saveVar[2]);
